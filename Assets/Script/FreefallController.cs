33b3d807ffd9c1836765131d00fe8f651603f736
earldenzel <earldenzel@gmail.com>
2017-09-30 16:41:34 -0400

    [ADD] Free fall Controller Script
    Separated free fall from movement. Should now move as intended.
    
    TODO:
    Add jumping

---
+++ Assets/Script/FreefallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreefallController : MonoBehaviour {
+    private RaycastHit2D hit1;
+    private RaycastHit2D hit2;
+    private RaycastHit2D hit3;
+    private Rigidbody2D tankVehicle;
+    private bool controllable;
+    private bool grounded;
+
+    public bool Controllable
+    {
+        get
+        {
+            return controllable;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        tankVehicle = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+    {
+        hit1 = Physics2D.Raycast(new Vector3(transform.GetChild(0).position.x, transform.GetChild(0).position.y - 0.5f), new Vector3(0, -1, 0), 0.05f);
+        hit2 = Physics2D.Raycast(new Vector3(transform.GetChild(1).position.x, transform.GetChild(1).position.y - 0.5f), new Vector3(0, -1, 0), 0.05f);
+        hit3 = Physics2D.Raycast(new Vector3(transform.GetChild(2).position.x, transform.GetChild(2).position.y), new Vector3(0, -1, 0), 0.05f);
+        
+        tankVehicle.AddForce(-20 * transform.up); // force that "sticks" the tank to the land
+        if(tankVehicle.rotation >90 || tankVehicle.rotation < -90 || (hit1.collider == null && hit2.collider == null && hit3.collider == null))
+        {
+            CorrectPosition();
+            controllable = false;
+        }
+        
+        if (hit1.collider != null || hit2.collider != null || hit3.collider != null)
+        {
+            controllable = true;
+        }
+    }
+
+    private void CorrectPosition()
+    {
+        transform.up = new Vector3(0, 1, 0);
+        tankVehicle.rotation = 0;
+
+    }
+}
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -6,73 +6,59 @@ public class Movement : MonoBehaviour {
     private Rigidbody2D tankVehicle;
     private RaycastHit2D hit;
     public float velocity = 2.0f;
-    private float childPositionX;
+    private bool rightDirection;
+    private Vector3 direction;
+    private Vector3 facingPosition;
 
 	// Use this for initialization
 	void Start () {
 		tankVehicle = GetComponent<Rigidbody2D>();
-        childPositionX = transform.GetChild(0).position.x;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (Input.GetAxis("Horizontal") != 0)
+
+        if (Input.GetAxis("Horizontal") != 0 && GetComponent<FreefallController>().Controllable)
+        //if (Input.GetAxis("Horizontal") != 0)
         {
             //change orientation of tank in respect to last directional command
-            if ((Input.GetAxis("Horizontal") < 0))
+            if ((Input.GetAxis("Horizontal") > 0))
             {
-                tankVehicle.GetComponent<SpriteRenderer>().flipX = true;
-                transform.GetChild(0).eulerAngles = new Vector3(0, 180, 0);
-                //transform.GetChild(0).position = new Vector3(-childPositionX, transform.GetChild(0).position.y, 0);
+                rightDirection = true;
+                tankVehicle.GetComponent<SpriteRenderer>().flipX = false;
+                facingPosition = transform.GetChild(0).position;
+                hit = Physics2D.Raycast(facingPosition, transform.right, 0.5f);
             }
             else
             {
-                tankVehicle.GetComponent<SpriteRenderer>().flipX = false;
-                transform.GetChild(0).eulerAngles = new Vector3(0, 0, 0);
-                //transform.GetChild(0).position = new Vector3(childPositionX, transform.GetChild(0).position.y, 0);
+                rightDirection = false;
+                tankVehicle.GetComponent<SpriteRenderer>().flipX = true;
+                facingPosition = transform.GetChild(1).position;
+                hit = Physics2D.Raycast(facingPosition, -transform.right, 0.5f);
             }
 
-            //detects if there is land in front of it       
-            hit = Physics2D.Raycast(transform.GetChild(0).position, transform.right, 0.5f);
+            //set direction for tank depending on where it's facing
+            if (rightDirection)
+            {
+                direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0) + transform.right - 0.5f * transform.up;
+            }
+            else
+            {
+                direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0) - transform.right - 0.5f * transform.up;
+            }
 
-            //if there is no land in front of tank, apply force normally
-            if (hit.collider == null)
+            //if there is a terrain collider in front of the tank, try to change direction upwards
+            if (hit.collider != null && hit.collider.tag == "Terrain")
             {
-                tankVehicle.AddForce(velocity * transform.right * Input.GetAxis("Horizontal"));
+                direction = direction + transform.up;
             }
-            //if there is indeed a piece of land, then try to apply an upward right force
-            else if (hit.collider.tag == "Terrain")
-            {
-                tankVehicle.AddForce(velocity * Vector2.one * Input.GetAxis("Horizontal"));
-                tankVehicle.rotation += 1f;
-                //tankVehicle.rotation += 0.1f;
-            }
+
+            //move depending on calculated direction
+            tankVehicle.velocity = velocity * direction;
         }
         else
         {
-            hit = Physics2D.Raycast(new Vector3(transform.position.x,transform.position.y-0.5f), new Vector3(0,-1,0),0.05f);
-            //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y - 0.33f), new Vector3(0, -1, 0));
-
-            //if vehicle is in a funny angle, the vehicle will right itself up
-            if(tankVehicle.rotation > 90 || tankVehicle.rotation <-90)
-            {
-                transform.up = new Vector3(0, 1, 0);
-                tankVehicle.freezeRotation = true;
-                tankVehicle.velocity = new Vector3(0, -100, 0);
-                transform.rotation = Quaternion.identity;
-                tankVehicle.freezeRotation = false;
-
-            }
-
-
-            //automatically stickies the vehicle to the ground when no movement occurs
-            tankVehicle.velocity = 5*-transform.up;
-
-            //if (hit.collider != null)
-            //{
-            //    Debug.Log(hit.collider.gameObject.name);
-            //    tankVehicle.velocity = -transform.up;
-            //}
+            tankVehicle.velocity = Vector3.zero; //if there is no directional input, set velocity to 0
         }
     }
 }
