f2ec2ca4e5693ce4caf99f159b81f9e471818613
TB-Terence <TB-Terence@users.noreply.github.com>
2017-10-12 12:14:31 -0400

    [ADD/UPDATE] Snow Mountain Map
    Add - Snow Mountain Map, along with the separate layers.
    Update - Camera Script(Allow the camera to zoom in/out or being fixed. The camera will follow the assigned target. The script is not completed yet, because we have to change the scripts that fires projectile)

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,19 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class CameraConfig
+{
+    public enum CameraState { ZoomIn, ZoomOut, Fixed };
+    public CameraState State;
+    public float MaxiumCameraSize;
+    public float MinimumCameraSize;
+    public float ZoomRatio;
+}
+
 public class CameraController : MonoBehaviour {
 
+    public enum Target { Player, Projectile };
+
+    public CameraConfig cameraConfig;
     public GameObject player;
+    public GameObject projectile;
+    public GameObject target;
+    public Target targetState;
     private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
-		
+        offset = new Vector3(0, 0, transform.position.z);
+        if (player != null)
+        {
+            target = player;
+        }
 	}
 	
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+        if (player != null && projectile != null)
+        {
+            switch (targetState)
+            {
+                case Target.Player:
+                    target = player;
+                    break;
+                case Target.Projectile:
+                    target = projectile;
+                    break;
+            }
+        }
+        switch (cameraConfig.State)
+        {
+            case CameraConfig.CameraState.ZoomIn:
+                Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - cameraConfig.ZoomRatio, cameraConfig.MinimumCameraSize, cameraConfig.MaxiumCameraSize);
+                break;
+            case CameraConfig.CameraState.ZoomOut:
+                Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize + cameraConfig.ZoomRatio, cameraConfig.MinimumCameraSize, cameraConfig.MaxiumCameraSize);
+                break;
+            case CameraConfig.CameraState.Fixed:
+                Camera.main.orthographicSize = cameraConfig.MaxiumCameraSize;
+                break;
+        }
+        transform.position = target.transform.position + offset;
+
+    }
 }
