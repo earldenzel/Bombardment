bce27cdb0a57707849f76c23503f004904cc7f88
earldenzel <earldenzel@gmail.com>
2017-10-18 09:56:47 -0400

    [Add] Deformable Ground Parts
    [ADD] Prefabs for blue ice, ground and snow. May not be the final
    prefabs
    [ADD] SpawnSnowStage script to spawn the Snow Mountain Stage
    [MODIFY] Folders. Separated prefabs between tank parts and deformable
    ground.
    [MODIFY] Scene. Removed temporary Terrain to start commencing work on
    deformable ground.
    
    Bugs:
    Movement is erratic and may need to be changed... again.
    Spawning too many objects may result in lag and stuff. Might be
    advisable to research Object Pooling to minimize performance?

---
+++ Assets/Script/SpawnSnowStage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSnowStage : MonoBehaviour {
+    public GameObject ice;
+    public GameObject snow;
+	// Use this for initialization
+	void Start () {
+        CreateBlock(ice, -2, -2, 30, 1);
+        CreateBlock(snow, -2, -3, 30, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateBlock(GameObject gameObject, int startx, int starty, float lengthx, float lengthy)
+    {
+        for(float i=0; i<lengthx; i += 0.08f)
+        {
+            for (float j=0; j<lengthy; j += 0.08f)
+            {
+                Instantiate(gameObject, new Vector3(startx+i, starty+j), Quaternion.identity);
+            }
+        }
+    }
+}
