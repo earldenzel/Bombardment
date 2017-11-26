fe0f6859af8fc252b7f2b31edef24f6a66a27513
TB-Terence <terencetang@hotmail.ca>
2017-11-26 17:56:03 -0500

    [Add] Scenes & Prefabs
    1. Added Loading Scene and Tank Selection Scene
    2. Added Power Up Prefabs(It does not apply the power ups yet)
    3. Added Helicopter deploy script
    4. Added Trail for Blue Arrow

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public enum GameScene { Loading, TankSelection, Main }
+
+    public GameScene NextScene;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (NextScene)
+        {
+            case GameScene.TankSelection:
+                if (Input.anyKey)
+                {
+                    Debug.Log("Loading Selection");
+                    SceneManager.LoadScene("selection");
+                }
+                break;
+            case GameScene.Main:
+                if (Input.GetKeyUp(KeyCode.KeypadEnter))
+                {
+                    Debug.Log("Loading Game");
+                    SceneManager.LoadScene("main");
+                }
+                break;
+        }
+	}
+}
