cb1e5f52412d036f540962c93481ebfa1666bf2a
TB-Terence <terencetang@hotmail.ca>
2017-11-26 18:44:48 -0500

    [Fix] Helicopter Script

---
+++ Assets/Scripts/HelicopterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterController : MonoBehaviour
+{
+
+    public Vector3 StartPoint;
+    public float Speed;
+    public float Interval = 1.0f;
+    public float MaxDropPerCycle;
+    public bool DepolyDrop;
+    public GameObject[] Drops;
+
+    private IEnumerator coroutine;
+
+    private int dropCount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = StartPoint;
+
+        coroutine = DepolyDrops(Interval);
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+
+    private IEnumerator DepolyDrops(float interval)
+    {
+        while (DepolyDrop)
+        {
+            yield return new WaitForSeconds(interval);
+            if (dropCount < MaxDropPerCycle)
+            {
+                GameObject drop = Drops[Random.Range(0, Drops.Length)];
+                if (drop != null)
+                {
+                    Instantiate(drop, this.transform);
+                    dropCount++;
+                }
+            }
+        }
+    }
+}
+
